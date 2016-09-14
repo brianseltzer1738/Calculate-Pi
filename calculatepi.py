19ed87c90feec2068880895102cfdbeed3161cd9
brianseltzer1738 <brianseltzer@hanovernorwichschools.org>
2016-09-14 13:14:48 -0400

    Updated from Brython Server: 9/14/2016 1:14:48 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Brian
+Credit: None
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
